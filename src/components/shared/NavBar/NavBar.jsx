6447392c336bde615ef5b5025894310d3ee9bae6
antu <antusaha970@gmail.com>
2023-04-26 20:11:18 +0600

    added navbar

--- src/components/shared/NavBar/NavBar.jsx
+++ src/components/shared/NavBar/NavBar.jsx
@@ -1,8 +1,16 @@
 import styled from "@emotion/styled";
-import { GitHub, LinkedIn } from "@mui/icons-material";
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { GitHub, LinkedIn, MenuOpen } from "@mui/icons-material";
+import {
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import { useState } from "react";
 
 const LogoText = styled(Typography)`
   background: linear-gradient(to right, #ff00cc, #333399);
@@ -13,6 +21,15 @@ const LogoText = styled(Typography)`
 `;
 
 const NavBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Container maxWidth="lg">
       <Stack
@@ -52,6 +69,68 @@ const NavBar = () => {
               <LinkedIn />
             </Link>
           </Stack>
+        </Box>
+        <Box display={{ lg: "none", md: "none", sm: "block", xs: "block" }}>
+          <IconButton
+            size="large"
+            aria-label="open website menubar"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <MenuOpen
+              sx={{
+                color: "#fff",
+                fontSize: "40px",
+              }}
+            />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <Stack
+              direction="column"
+              justifyContent="center"
+              gap={2}
+              pl={1}
+              color="#000"
+            >
+              <Link to="" className="nav-link">
+                Home
+              </Link>
+              <Link to="" className="nav-link">
+                About
+              </Link>
+              <Link to="" className="nav-link">
+                Tech Stack
+              </Link>
+              <Link to="" className="nav-link">
+                Projects
+              </Link>
+              <Link to="" className="nav-link">
+                Contact
+              </Link>
+              <Link to="" className="nav-link">
+                <GitHub />
+              </Link>
+              <Link to="" className="nav-link">
+                <LinkedIn />
+              </Link>
+            </Stack>
+          </Menu>
         </Box>
       </Stack>
     </Container>
