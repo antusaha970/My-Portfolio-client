ec7a6cd79cf79b73575232a0d7857e10dd2971d1
antu <antusaha970@gmail.com>
2023-04-30 20:54:34 +0600

    added login component

--- src/components/AdminPage/AdminLogin/AdminLogin.jsx
+++ src/components/AdminPage/AdminLogin/AdminLogin.jsx
@@ -1,13 +1,97 @@
-import { Box, Container } from "@mui/material";
+import styled from "@emotion/styled";
+import { Box, Button, Container, TextField } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
+
+const CssTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#fff",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#fff",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "gray",
+    },
+    "&:hover fieldset": {
+      borderColor: "green",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#fff",
+    },
+  },
+});
 
 const AdminLogin = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
   return (
     <Box
       sx={{
         minHeight: "90vh",
       }}
     >
-      <Container maxWidth="lg"></Container>
+      <Container maxWidth="lg">
+        <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <CssTextField
+                {...field}
+                id="emailInp"
+                label="Enter Your Email"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                InputProps={{
+                  style: {
+                    color: "#fff",
+                  },
+                }}
+                InputLabelProps={{
+                  style: {
+                    color: "#fff",
+                  },
+                }}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <CssTextField
+                {...field}
+                id="passInp"
+                label="Enter Your Password"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                InputProps={{
+                  style: {
+                    color: "#fff",
+                  },
+                }}
+                InputLabelProps={{
+                  style: {
+                    color: "#fff",
+                  },
+                }}
+                type="password"
+              />
+            )}
+          />
+          <Button type="submit" variant="contained">
+            Login
+          </Button>
+        </Box>
+      </Container>
     </Box>
   );
 };
