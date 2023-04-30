d55896f4f0eae36dd7ac9b0a84cf218c9c0bd67b
antu <antusaha970@gmail.com>
2023-04-30 22:55:58 +0600

    added admin menu

--- src/components/AdminPage/AdminMenu/AdminMenu.jsx
+++ src/components/AdminPage/AdminMenu/AdminMenu.jsx
@@ -1,5 +1,38 @@
+import styled from "@emotion/styled";
+import { Box, Button, Container, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const CustomButton = styled(Button)`
+  && {
+    & > a {
+      color: #fff;
+    }
+  }
+`;
+
 const AdminMenu = () => {
-  return <div>AdminMenu</div>;
+  return (
+    <Box
+      sx={{
+        minHeight: "90vh",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Stack
+          sx={{
+            minHeight: "80vh",
+            minWidth: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CustomButton color="secondary" variant="contained">
+            <Link to="/admin/add_project">Add Project</Link>
+          </CustomButton>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
 
 export default AdminMenu;
