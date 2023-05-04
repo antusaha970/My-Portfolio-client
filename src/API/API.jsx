2201b12f4ed299e9b4d27780e772d071160fe05b
antu <antusaha970@gmail.com>
2023-05-04 20:45:36 +0600

    updated api link

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const client = axios.create({
-  // baseURL: "https://portfolio-of-antu.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://portfolio-of-antu.onrender.com/",
+  // baseURL: "http://localhost:5000/",
 });
 
 export default client;
